7a5926bab08cdea5749164fe5c4d437a8f51e82b
yasirjanjua <yasir_janjua@hotmail.com>
2020-05-25 01:04:51 +0500

    feat: update logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath="yasirjanjua/machine-learning-services-kubernetes:lastest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment udacity-project-4 --image=$dockerpath
+kubectl create deployment udacity-ml-project --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
@@ -17,5 +17,5 @@ kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment udacity-project-4 --type="NodePort" --port=80
-kubectl port-forward deployment/udacity-project-4 8000:80
+kubectl expose deployment udacity-ml-project --type="NodePort" --port=80
+kubectl port-forward deployment/udacity-ml-project 8000:80
